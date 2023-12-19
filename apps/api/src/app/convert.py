528b2cca64fb5569890f151fe198058ab0f1452e
Juanfran <juanfran.ag@gmail.com>
2023-12-19 17:21:09 +0100

    feat: generate video preview

--- apps/api/src/app/convert.py
+++ apps/api/src/app/convert.py
@@ -4,6 +4,7 @@ import sys
 import os
 import whisperx
 import gc
+from PIL import Image
 
 arguments = sys.argv
 
@@ -15,13 +16,22 @@ token = arguments[3]
 uploads = "uploads/"
 folder = uploads + audioId + "/"
 audio = folder + audioId + ".wav"
+video = folder + audioId + ".webm"
+frame_path = folder + audioId + ".jpg"
 
 # speakers = folder + audioId + "-speakers.json"
 srt = folder + audioId + "-whisper.json"
 
 # file to > wav
 clip = moviepy.VideoFileClip(source)
+clip.write_videofile(video,fps=25)
 clip.audio.write_audiofile(audio)
+
+frame = clip.get_frame(int(clip.duration) / 2)
+image = Image.fromarray(frame)
+image.save(frame_path)
+
+os.remove(source)
 
 # Init whisper
 device = "cuda"
@@ -34,13 +44,10 @@ model = whisperx.load_model("large-v2", device, compute_type=compute_type)
 
 audio = whisperx.load_audio(audio_file)
 result = model.transcribe(audio, batch_size=batch_size)
-# print(result["segments"]) # before alignment
 
 # 2. Align whisper output
 model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
 result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
-
-# print(result["segments"]) # after alignment
 
 # 3. Assign speaker labels
 diarize_model = whisperx.DiarizationPipeline(use_auth_token=token, device=device)
@@ -53,6 +60,7 @@ result_json_string = json.dumps(result["segments"], indent=2)
 with open(srt, 'w') as outfile:
     outfile.write(result_json_string)
 
+# 4. Create metadata
 speakers = set()
 
 for whisper in result["segments"]:
